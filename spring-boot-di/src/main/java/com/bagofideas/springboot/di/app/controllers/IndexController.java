a89fce87df7beef8e528f5c5e732bb0be1c44e08
Maicol Orejuela <viafara96@hotmail.com>
2022-05-18 21:03:20 -0500

    Add manual service

--- spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/controllers/IndexController.java
+++ spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.bagofideas.springboot.di.app.controllers;
 
+import com.bagofideas.springboot.di.app.models.services.MyService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController
 {
+    private MyService service = new MyService();
+
     @GetMapping({"/", "", "/index"})
     public String index(Model model)
     {
+        model.addAttribute("object", service.operation());
         return "index";
     }
 }
---
+++ spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/models/services/MyService.java
@@ -0,0 +1,9 @@
+package com.bagofideas.springboot.di.app.models.services;
+
+public class MyService
+{
+    public String operation()
+    {
+        return "execute important process";
+    }
+}
