94c9879ffb32ab8d3b881064ca62c85cd2014b35
Maicol Orejuela <viafara96@hotmail.com>
2022-05-20 15:08:34 -0500

    @Primary define duplicity in interfaces

--- spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/services/MyService.java
+++ spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/services/MyService.java
@@ -1,13 +1,15 @@
 package com.bagofideas.springboot.di.app.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component("MySimpleService")
 public class MyService implements IService
 {
     @Override
     public String operation()
     {
-        return "execute important process";
+        return "execute important simple process";
     }
 }
---
+++ spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/services/MyServiceComplex.java
@@ -0,0 +1,13 @@
+package com.bagofideas.springboot.di.app.services;
+
+import org.springframework.stereotype.Component;
+
+@Component("MyComplexService")
+public class MyServiceComplex implements IService
+{
+    @Override
+    public String operation()
+    {
+        return "execute important complex process";
+    }
+}
