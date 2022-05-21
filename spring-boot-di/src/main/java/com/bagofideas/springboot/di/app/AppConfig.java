30aa7398d9801552e02d1e6c6866d2ef5dc63f57
Maicol Orejuela <viafara96@hotmail.com>
2022-05-21 11:49:49 -0500

    Other bean

--- spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/AppConfig.java
+++ spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/AppConfig.java
@@ -34,10 +34,27 @@ public class AppConfig
         Product product1 = new Product("Sony camera", 100.00);
         Product product2 = new Product("Bicycle Bianchi aro 26", 200.00);
 
-        Item linea1 = new Item(product1, 4);
-        Item linea2 = new Item(product2, 2);
+        Item line1 = new Item(product1, 4);
+        Item line2 = new Item(product2, 2);
 
-        return Arrays.asList(linea1,linea2);
+        return Arrays.asList(line1,line2);
+    }
+
+    @Primary
+    @Bean("invoiceItemsOffice")
+    public List<Item> registerItemsOffice()
+    {
+        Product product1 = new Product("Monitor LG LCD 24", 250.70);
+        Product product2 = new Product("Notebook asus", 300.56);
+        Product product3 = new Product("Multifunctional printer HP", 80.01);
+        Product product4 = new Product("Office desktop", 100.50);
+
+        Item line1 = new Item(product1, 2);
+        Item line2 = new Item(product2, 2);
+        Item line3 = new Item(product3, 1);
+        Item line4 = new Item(product4, 1);
+
+        return Arrays.asList(line1, line2,line3,line4);
     }
 
 }
