a449b00039af080d3fc0402c9c00afee946501e0
Maicol Orejuela <viafara96@hotmail.com>
2022-05-20 23:51:22 -0500

    Create new beans and components for use later

--- spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/domain/Item.java
+++ spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/domain/Item.java
@@ -4,4 +4,10 @@ public class Item
 {
     private Product product;
     private Integer quantity;
+
+    public Item(Product product, Integer quantity)
+    {
+        this.product = product;
+        this.quantity = quantity;
+    }
 }
--- spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/domain/Product.java
+++ spring-boot-di/src/main/java/com/bagofideas/springboot/di/app/domain/Product.java
@@ -4,4 +4,10 @@ public class Product
 {
     private String name;
     private Double price;
+
+    public Product(String name, Double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
 }
