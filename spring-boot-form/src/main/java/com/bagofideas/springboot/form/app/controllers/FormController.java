df8012d5b239a127ac51114cb0bf92f69ed68bbd
Maicol Orejuela <viafara96@hotmail.com>
2022-05-24 19:20:41 -0500

    Complete controller and result view

--- spring-boot-form/src/main/java/com/bagofideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bagofideas/springboot/form/app/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController
@@ -11,12 +12,20 @@ public class FormController
     @GetMapping("/form")
     public String form(Model model)
     {
+        model.addAttribute("title", "Users form");
         return "form";
     }
 
     @PostMapping("/form")
-    public String process(Model model)
+    public String process(Model model,
+                          @RequestParam(name = "userName") String userName,
+                          @RequestParam String password,
+                          @RequestParam String email)
     {
+        model.addAttribute("title", "Form result");
+        model.addAttribute("userName", userName);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
         return "result";
     }
 }
