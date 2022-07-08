af27f14a0cdc3a1a51ec5c73777349e0017371ec
Maicol Orejuela <viafara96@hotmail.com>
2022-07-07 19:39:33 -0500

    Start project JPA -@Entity -DAO -H2 Console -@Transactional
    import sql
    html edit
    messages properties
    validations with annotations
    css bootstrapp

--- spring-boot-data-jpa/src/main/java/com/bagofideas/springboot/datajpa/app/controllers/CustomerController.java
+++ spring-boot-data-jpa/src/main/java/com/bagofideas/springboot/datajpa/app/controllers/CustomerController.java
@@ -1,12 +1,83 @@
 package com.bagofideas.springboot.datajpa.app.controllers;
 
+import com.bagofideas.springboot.datajpa.app.models.dao.ICustomerDao;
+import com.bagofideas.springboot.datajpa.app.models.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 @SessionAttributes("Customer")
 public class CustomerController
 {
+    @Autowired
+    private ICustomerDao customerDao;
+
+    @RequestMapping (value = "/toList", method = RequestMethod.GET)
+    public String ToList(Model model)
+    {
+        model.addAttribute("title", "Customer list");
+        model.addAttribute("customers", customerDao.findAll());
+        return "toList";
+    }
+
+    @RequestMapping(value = "/form")
+    public String toCreate (Map<String, Object> model)
+    {
+        Customer customer = new Customer();
+        model.put("customer",customer);
+        model.put("title","Customer form");
+        return "form";
+    }
+
+    @RequestMapping(value = "/form/{id}")
+    public String edit(@PathVariable(value = "id") Long id, Map<String, Object> model)
+    {
+        Customer customer = null;
+        if(id>0)
+        {
+             customer = customerDao.findOne(id);
+        }
+        else
+        {
+            return "redirect:/toList";
+        }
+        model.put("customer",customer);
+        model.put("title","Customer edit");
+        return "form";
+    }
+
+    @RequestMapping (value = "/form", method = RequestMethod.POST)
+    public String save(@Valid Customer customer, BindingResult result, Model model, SessionStatus status)
+    {
+        if(result.hasErrors())
+        {
+            model.addAttribute("title", "Customer form");
+            return "form";
+        }
+        customerDao.save(customer);
+        status.setComplete();
+        return "redirect:toList";
+    }
+
+    @RequestMapping(value = "/delete/{id}")
+    public String delete(@PathVariable(value = "id") Long id)
+    {
+        if(id > 0)
+        {
+            customerDao.delete(id);
+        }
+        return "redirect:/toList";
+    }
 
 }
 
---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO customers (id, name, lastName, email, createDate) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
+INSERT INTO customers (id, name, lastName, email, createDate) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
